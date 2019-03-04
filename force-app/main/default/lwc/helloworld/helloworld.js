a04be762dc26c269c7851501de1e9e1b39ab716c
PAKHIRA <priyankar.pakhira@atos.net>
2019-03-04 14:09:27 +0000

    4th commit

--- force-app/main/default/lwc/helloworld/helloworld.js
+++ force-app/main/default/lwc/helloworld/helloworld.js
@@ -1,6 +1,8 @@
 import { LightningElement, track } from 'lwc';
-export default class HelloWorld extends LightningElement {
+export default class HelloWorld extends LightningElement
+ {
     @track greeting = 'World'; 
     @track message='Hello';
+    @track message1='Hello3';
      
 }
